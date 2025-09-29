b273034629a3c26d7f20161f0049c9a6392a77f2
aminullah-dev <Aminhashemi979@gmail.com>
2025-09-29 13:47:53 -0400

    Add myConsoleProject as a normal folder

---
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re \"accounting\" on you!");
+
+            Console.ReadLine();
+        }
+    }
+}
