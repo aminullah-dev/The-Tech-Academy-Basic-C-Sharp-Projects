cc1061a3f8bb0630af8a38a3f1057c804872fa5f
aminullah-dev <Aminhashemi979@gmail.com>
2025-11-04 14:38:55 -0500

    Add CallingMethodsAssignment project (methods with class & simple comments)

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// This class has math methods
+public class MathOperations
+{
+    // This method multiplies the number by 2
+    public int MultiplyByTwo(int number)
+    {
+        return number * 2;
+    }
+
+    // This method squares the number (number * number)
+    public int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // This method adds 10 to the number
+    public int AddTen(int number)
+    {
+        return number + 10;
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+
+        MathOperations mathOps = new MathOperations();
+
+        int result1 = mathOps.MultiplyByTwo(userNumber);
+        Console.WriteLine("Your number multiplied by 2 is: " + result1);
+
+        int result2 = mathOps.SquareNumber(userNumber);
+        Console.WriteLine("Your number squared is: " + result2);
+
+        int result3 = mathOps.AddTen(userNumber);
+        Console.WriteLine("Your number plus 10 is: " + result3);
+
+        Console.WriteLine("All done! Press any key to exit.");
+        Console.ReadKey();
+    }
+}
