1f5aa2839791269884fd092191dd465d714bdc35
aminullah-dev <Aminhashemi979@gmail.com>
2025-11-04 16:26:29 -0500

    Add VoidMethodAssignment project (void method with two int parameters and named call)

---
+++ VoidMethodAssignment/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    // This class has our void method
+    class MathOperations
+    {
+        // This is a void method that takes two integers
+        public void DoMath(int number1, int number2)
+        {
+            // Multiply the first number by 2
+            int result = number1 * 2;
+
+            // Show the result
+            Console.WriteLine("Result of first number * 2 is: " + result);
+
+            // Show the second number
+            Console.WriteLine("The second number is: " + number2);
+        }
+    }
+}
---
+++ VoidMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of MathOperations class
+            MathOperations math = new MathOperations();
+
+            // Call method with two numbers
+            math.DoMath(5, 10);
+
+            // Call method again, using named parameters
+            math.DoMath(number1: 8, number2: 3);
+
+            // Keep window open
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
